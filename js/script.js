55118196394f0f380d0cff1e65283569fea894ff
nisargs <nisarg2198@gmail.com>
2020-07-15 21:16:10 +0530

    Added Caraousel for Skills

--- js/script.js
+++ js/script.js
@@ -13,4 +13,34 @@ $(document).ready(function () {
         showCursor: false
     });
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent){
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+        });
+
 });
